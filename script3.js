b4c5680aaebc3dfb0c01a40f11b78269de08fd96
unknown <saharphantom@yahoo.com>
2018-11-21 15:19:38 +0330

    avale start

--- script3.js
+++ script3.js
@@ -1,58 +1,67 @@
-var golIndex=Math.floor(Math.random() * (4 - 1) ) + 1;
-window.onload=function(){
-    var imagelist=document.getElementsByClassName("aimg");
-    var x=document.getElementsByClassName("golimg");
-    var u=document.getElementsByClassName("underimg");
-    
-    var a=false;
-    for(let i=0;i<3;i++)
-    {
-        
+var u, x, imagelist;
+var isFirstTime=true;
+var golIndex = Math.floor(Math.random() * (4 - 1)) + 1;
+window.onload = function () {
+    initMatch();
+}
+function initMatch() {
+    imagelist = document.getElementsByClassName("aimg");
+    x = document.getElementsByClassName("golimg");
+    u = document.getElementsByClassName("underimg");
+    initResult();
 
-        if(golIndex-1==i)
-        {
-            x[golIndex-1].src="img/pokeball.svg";
+
+}
+function initResult() {
+    for (let i = 0; i < 3; i++) {
+        if (golIndex - 1 == i) {
+            x[golIndex - 1].src = "img/pokeball.svg";
         }
-        else
-        {
-            x[i].src="img/fist.svg";  
+        else {
+            x[i].src = "img/fist.svg";
         }
-        
-            imagelist[i].addEventListener("click",function(event){
-                
-                switch(i){
-                    case 0:
-                    u[0].style.display="block";
-                    // u[0].classList.add("show");
-                    break;
-                    case 1:
-                    u[1].style.display="block";
-                    // u[1].classList.add("show");
-                    break;
-                    case 2:
-                    u[2].style.display="block";
-                    // u[2].classList.add("show");
-                 }
-            imagelist[0].classList.add("hide");
-            imagelist[1].classList.add("hide");
-            imagelist[2].classList.add("hide");
-            imagelist[i].disabled = true;
-            
-             
+        imagelist[i].addEventListener("click", function (event) {
+
+            if(isFirstTime){
+                showResult(i);
+            isFirstTime=false;
+            }
+
+
         })
-        
-     
-        
     }
 }
-$=function(input){
-  var dom=document.querySelectorAll(input);
-    return dom;
-}
-NodeList.prototype.addClass=function(className){
-    debugger;
-    for(var i=0;i<this.length;i++)
-    {
-        this[i].classList.add(className);
+
+function showResult(clickedItemIndex) {
+
+        switch (clickedItemIndex) {
+            case 0:
+                u[0].style.display = "block";
+                // u[0].classList.add("show");
+                break;
+            case 1:
+                u[1].style.display = "block";
+                // u[1].classList.add("show");
+                break;
+            case 2:
+                u[2].style.display = "block";
+            // u[2].classList.add("show");
+        }
+        document.getElementById("aimg1").classList.add("--upper");
+        document.getElementById("aimg2").classList.add("--upper");
+        document.getElementById("aimg3").classList.add("--upper");
+
+
+
+
     }
+function gameStart(){
+        document.getElementById("aimg1").classList.remove("--upper");
+        document.getElementById("aimg2").classList.remove("--upper");
+        document.getElementById("aimg3").classList.remove("--upper");
+        
 }
+
+
+
+
